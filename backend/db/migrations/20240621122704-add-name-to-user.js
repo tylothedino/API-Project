2cd6061cfcb1672c3d3e070e8b2d806e7dfb843d
tyler <tylothedino@gmail.com>
2024-06-21 05:59:36 -0700

    added options to addname migration p2

--- backend/db/migrations/20240621122704-add-name-to-user.js
+++ backend/db/migrations/20240621122704-add-name-to-user.js
@@ -6,31 +6,16 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
-/** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-
-    await queryInterface.addColumn('Users', 'firstName', { type: Sequelize.STRING });
+    await queryInterface.addColumn('Users', 'firstName', { type: Sequelize.STRING }, options);
     await queryInterface.addColumn('Users', 'lastName', { type: Sequelize.STRING }, options);
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
     options.tableName = "Users";
     return queryInterface.removeColumn(options);
-
-
 
 
   }
