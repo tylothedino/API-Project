d55c91e5243f7c69d353a7b71436fc74e4ada49a
tyler <tylothedino@gmail.com>
2024-06-21 04:36:09 -0700

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
 /*
     -- INITIAL TEST ROUTE --
@@ -20,6 +21,6 @@ router.get("/api/csrf/restore", (req, res) => {
     });
 });
 
-
+router.use('/api', apiRouter);
 
 module.exports = router;
