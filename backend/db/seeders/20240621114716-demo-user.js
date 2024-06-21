4ffaafb5b6ef77a8ff8b647c56a5f7c6a4612d95
tyler <tylothedino@gmail.com>
2024-06-21 06:16:36 -0700

    Name works and updated Seeders

--- backend/db/seeders/20240621114716-demo-user.js
+++ backend/db/seeders/20240621114716-demo-user.js
@@ -15,16 +15,22 @@ module.exports = {
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password'),
+        firstName: "John",
+        lastName: "Legend"
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2'),
+        firstName: "Gabriel",
+        lastName: "Michael"
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3'),
+        firstName: "Tyler",
+        lastName: "Swarez"
       }
     ], { validate: true });
   },
