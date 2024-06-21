03c6855be61708563736d6427cc061393d2cebe2
tyler <tylothedino@gmail.com>
2024-06-21 02:46:10 -0700

    Initialize Express and Sequelize with CSRF protection

---
+++ backend/psql-setup-script.js
@@ -0,0 +1,9 @@
+//Used in PRODUCTION to allow use of SCHEMA
+
+const { sequelize } = require('./db/models');
+
+sequelize.showAllSchemas({ logging: false }).then(async (data) => {
+    if (!data.includes(process.env.SCHEMA)) {
+        await sequelize.createSchema(process.env.SCHEMA);
+    }
+});
