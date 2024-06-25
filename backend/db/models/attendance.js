2e5b42b8d1cc82bc73f8f1eec951299ffa8a8112
tyler <tylothedino@gmail.com>
2024-06-25 00:46:40 -0700

    Edited foreign keys

--- backend/db/models/attendance.js
+++ backend/db/models/attendance.js
@@ -12,7 +12,11 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Attendance.belongsTo(
-        models.User, { foreignKey: 'userId' }
+        models.User,
+        {
+          foreignKey: 'userId',
+          onDelete: 'CASCADE'
+        }
       );
 
       Attendance.belongsTo(
