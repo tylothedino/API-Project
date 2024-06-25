e0ed45706ee0a645be140e3c28ee80a7f81bace8
tyler <tylothedino@gmail.com>
2024-06-25 03:37:50 -0700

    Completed Group setup

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -4,6 +4,7 @@ const { Model, Validator } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class Group extends Model {
+    //User Association
     static associate(models) {
       Group.belongsTo(models.User, {
         foreignKey: 'organizerId'
