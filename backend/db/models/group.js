3da1cfcbd054087bc6adfbc5f5cad7da1d590a3d
tyler <tylothedino@gmail.com>
2024-06-25 09:40:22 -0700

    Added model and migrations for groupImages

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -19,6 +19,9 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'groupId', onDelete: 'CASCADE', hooks: true
       });
 
+      Event.hasMany(models.GroupImage, {
+        foreignKey: 'groupId', onDelete: 'CASCADE', hooks: true
+      });
 
     }
   }
