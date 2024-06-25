b8e36b11fc16958d9de010e3b28cde6f0bb36c25
tyler <tylothedino@gmail.com>
2024-06-25 02:12:57 -0700

    Redid Users

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,12 +1,13 @@
-'use strict';
-const { Model, Validator } = require('sequelize');
+"use strict";
+
+const { Model, Validator } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
-      // define association here
+
     }
-  };
+  }
 
   User.init(
     {
--- backend/db/seeders/20240621114716-demo-user.js
+++ backend/db/seeders/20240621114716-demo-user.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const { User } = require("../models");
 const bcrypt = require("bcryptjs");
 
