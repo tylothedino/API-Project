b0bd10b6ac7968e44e12afa9a5c8d1a380fb9b26
tyler <tylothedino@gmail.com>
2024-07-16 02:54:06 -0700

    Added some details for Groups - Missing events

--- frontend/src/components/Groups/SingleGroup.jsx
+++ frontend/src/components/Groups/SingleGroup.jsx
@@ -9,7 +9,7 @@ function SingleGroup() {
     const { groupId } = useParams();
 
     const group = useSelector((state) => state.group[groupId]);
-    const sessionUser = useSelector((state) => state.session.user);
+
 
     const dispatch = useDispatch();
     //Initialize the groups
@@ -21,31 +21,52 @@ function SingleGroup() {
     // console.log(group)
 
     const groupImage = group && group.GroupImages && group.GroupImages.find((image) => image.preview);
-
+    console.log("GROUPIMAGE: ", groupImage)
 
 
     return (
-        <div className='container'>
-            <div className='header'>
-                <p>
-                    &lt; <NavLink className='groupNav' to='/groups'>Groups</NavLink>
-                </p>
+        <div>
+            <div className='container'>
+                <div className='header'>
+                    <p>
+                        &lt; <NavLink className='groupNav' to='/groups'>Groups</NavLink>
+                    </p>
 
-                <div className='singleGroupContent'>
-                    <div className='groupImage'>
-                        <img src={groupImage && `${groupImage.url}`}></img>
-                    </div>
-                    <div className='groupContent'>
-                        <h2 className='groupName'>{group && group.name}</h2>
-
-                        <div className='groupDesc'>
-                            <h3>{group && group.city + ", " + group.state}</h3>
-                            <h3>{group && group.eventCount} event(s) · {group && group.private ? " Private" : " Public"}</h3>
-                            <h3>Organized by {group && group.Organizer && (group.Organizer.firstName + " " + group.Organizer.lastName)}</h3>
+                    <div className='singleGroupContent'>
+                        <div className='groupImage'>
+                            {groupImage ? <img src={groupImage && `${groupImage.url}`}></img> : <img src="null"></img>}
                         </div>
+                        <div className='groupContent'>
+                            <h2 className='groupName'>{group && group.name}</h2>
 
+                            <div className='groupDesc'>
+                                <h3>{group && group.city + ", " + group.state}</h3>
+                                <h3>{group && group.eventCount} event(s) · {group && group.private ? " Private" : " Public"}</h3>
+                                <h3>Organized by {group && group.Organizer && (group.Organizer.firstName + " " + group.Organizer.lastName)}</h3>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
+            </div>
+
+            <div className='detailsContainer'>
+                <div className='details'>
+                    <h2 className='organizerHeader'>
+                        Organizer
+                        <p className='groupDesc'>
+                            {group && group.Organizer && (group.Organizer.firstName + " " + group.Organizer.lastName)}
+                        </p>
+                    </h2>
+
+                    <h2 className='organizerHeader'>
+                        What we're about
+                        <p className='groupInfo'>
+                            {group && group.about}
+                        </p>
+                    </h2>
+                </div>
+
             </div>
         </div>
 
